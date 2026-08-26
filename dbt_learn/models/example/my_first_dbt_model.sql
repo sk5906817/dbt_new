ea3cc3345ebb3cbad2d3221643557ed84e8d81d2
Shubham Singh <sk5906817@gmail.com>
2026-08-26 13:55:06 +0530

    Data

--- dbt_learn/models/example/my_first_dbt_model.sql
+++ dbt_learn/models/example/my_first_dbt_model.sql
@@ -7,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table',alias='first_model') }}
+{{ config(materialized='table', alias='first_model', post_hook="grant select on " ~ this ~ " to role analytics") }}
 
 with source_data as (
 
